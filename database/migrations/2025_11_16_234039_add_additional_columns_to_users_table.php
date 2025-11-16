44137438de12e146f53e7b2e3d6eafed13eb2f3e
Felipe Silva <felipe.allware@gmail.com>
2025-11-16 20:47:50 -0300

    feat: Criando migration para completar a tabela users

---
+++ database/migrations/2025_11_16_234039_add_additional_columns_to_users_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->string('username', 80)->nullable();
+            $table->string('phone_number', 14)->nullable();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->dropColumn([
+                'username',
+                'phone_number',
+                'deleted_at'
+            ]);
+        });
+    }
+};
