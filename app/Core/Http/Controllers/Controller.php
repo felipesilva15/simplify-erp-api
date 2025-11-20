c5eb71cc4e26782642cba0eac1ff729447cf78dd
Felipe Silva <felipe.allware@gmail.com>
2025-11-20 13:31:14 -0300

    feat: Definição da base da documentação no Swagger

---
+++ app/Core/Http/Controllers/Controller.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Core\Http\Controllers;
+
+/**
+ * @OA\Info(
+ *      title="Simplify ERP API",
+ *      version="1.0.0",
+ *      description="A sessão abaixo contém a documentação da API utilizada para o Simplify ERP.",
+ *      @OA\Contact(
+ *          email="felipe.allware@gmail.com",
+ *          name="Felipe Silva"
+ *      ),
+ *      @OA\License(
+ *          name="Licença API",
+ *          url="https://github.com/felipesilva15/simplify-erp-api/blob/main/LICENSE"
+ *      )
+ * )
+ */
+abstract class Controller
+{
+    
+}
