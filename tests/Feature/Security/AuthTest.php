00279389ba1c57bba205bc021a52c76a64010718
felipe.silva <felipe.silva@pwi.com.br>
2026-01-13 22:54:09 -0300

    test: Criando testes para recursos de autenticaçãog

---
+++ tests/Feature/Security/AuthTest.php
@@ -0,0 +1,130 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Modules\Security\Models\User;
+use Illuminate\Http\Response;
+use Illuminate\Support\Facades\Auth;
+use Tests\TestCase;
+
+class AuthTest extends TestCase
+{
+    protected string $endpoint = '/api/security/auth';
+
+    private function getCredentials(): array {
+        $user = User::factory()->createOne();
+
+        return [
+            'username' => $user->username,
+            'password' => User::factory()->getDefaultPassword(),
+        ];
+    }
+
+    public function test_can_login(): void
+    {
+        $data = $this->getCredentials();
+        $response = $this->postJson("{$this->endpoint}/login", $data);
+
+        $response->assertStatus(Response::HTTP_OK)
+                ->assertJsonIsObject()
+                ->assertJsonStructure([
+                    'access_token',
+                    'token_type',
+                    'expires_in'
+                ]);
+    }
+
+    public function test_cannot_login_with_invalid_credentials(): void
+    {
+        $data = $this->getCredentials();
+        $data['password'] = "123456789";
+        $response = $this->postJson("{$this->endpoint}/login", $data);
+
+        $this->assertErrorResponse($response, Response::HTTP_UNAUTHORIZED);
+    }
+
+    public function test_can_logout(): void
+    {
+        $response = $this->postJson("{$this->endpoint}/logout", [], $this->getAdminAuthHeaders());
+        $response->assertNoContent();
+    }
+
+    public function test_can_refresh_token(): void
+    {
+        $response = $this->postJson("{$this->endpoint}/refresh", [], $this->getAdminAuthHeaders());
+
+        $response->assertStatus(Response::HTTP_OK)
+                ->assertJsonIsObject()
+                ->assertJsonStructure([
+                    'access_token',
+                    'token_type',
+                    'expires_in'
+                ]);
+    }
+
+    public function test_can_get_data_logged_in_user(): void
+    {
+        $response = $this->getJson("{$this->endpoint}/me", $this->getAdminAuthHeaders());
+
+        $response->assertStatus(Response::HTTP_OK)
+                ->assertJsonIsObject()
+                ->assertJsonStructure([
+                    'id',
+                    'name',
+                    'username',
+                    'permissions'
+                ])
+                ->assertJsonFragment([
+                    'id' => $this->adminAuthUser->id,
+                    'name' => $this->adminAuthUser->name,
+                    'username' => $this->adminAuthUser->username,
+                ]);
+    }
+
+    public function test_invalid_token_fails(): void
+    {
+        $headers = [
+            'Authorization' => 'Bearer invalid_token'
+        ];
+
+        $response = $this->getJson("{$this->endpoint}/me", $headers);
+
+        $this->assertErrorResponse($response, Response::HTTP_UNAUTHORIZED);
+    }
+
+    public function test_expired_token_fails(): void
+    {
+        $data = $this->getCredentials();
+        $response = $this->postJson("{$this->endpoint}/login", $data);
+
+        $expiresIn = $response->json('expires_in');
+        $token = $response->json('access_token');
+
+        $this->travel($expiresIn)->minutes();
+
+        Auth::forgetUser();
+
+        $response = $this->getJson("{$this->endpoint}/me", [
+            "Authorization" => "Bearer ".$token
+        ]);
+
+        $this->assertErrorResponse($response, Response::HTTP_UNAUTHORIZED);
+    }
+
+    public function test_can_use_valid_token_successful(): void
+    {
+        $data = $this->getCredentials();
+        $response = $this->postJson("{$this->endpoint}/login", $data);
+
+        $token = $response->json('access_token');
+
+        Auth::forgetUser();
+
+        $response = $this->getJson("{$this->endpoint}/me", [
+            "Authorization" => "Bearer ".$token
+        ]);
+
+        $response->assertStatus(Response::HTTP_OK)
+                ->assertJsonIsObject();
+    }
+}
