113e013c092c36557e66f98be4656bf65395b316
felipe.silva <felipe.silva@pwi.com.br>
2026-01-16 13:56:38 -0300

    refactor: Retirando código desnecessário

--- app/Modules/Security/DTO/TokenDetailsDTO.php
+++ app/Modules/Security/DTO/TokenDetailsDTO.php
@@ -9,20 +9,4 @@ class TokenDetailsDTO
         public string $type = '',
         public int $expiresIn = 0
     ) { }
-
-    public static function fromArray(array $data): self {
-        return new self(
-            token: $data['token'] ?? '',
-            type: $data['type'] ?? '',
-            expiresIn: $data['expiresIn'] ?? 0
-        );
-    }
-
-    public function toArray(): array {
-        return [
-            'token' => $this->token,
-            'type' => $this->type,
-            'expiresIn' => $this->expiresIn
-        ];
-    }
 }
